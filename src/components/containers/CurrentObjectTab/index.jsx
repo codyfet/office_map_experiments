ba4cbd7ef6aa42549357e4d70b426d5250095075
TimmyGOGO <temich941@mail.ru>
2019-01-31 11:49:25 +0300

    Now we can change/attach a user to any table with checking the user is free

--- src/components/containers/CurrentObjectTab/index.jsx
+++ src/components/containers/CurrentObjectTab/index.jsx
@@ -1,107 +1,135 @@
-import * as React from 'react';
-import ObjectItem from '../../containers/ObjectItem/index';
-import UserSpecialItem from '../../containers/UserSpecialItem/index';
-import UsersSpecialList from '../../containers/UsersSpecialList/index';
-import CurrentObjectItem from '../../containers/CurrentObjectItem/index';
+import * as React from "react";
+import ObjectItem from "../../containers/ObjectItem/index";
+import UserSpecialItem from "../../containers/UserSpecialItem/index";
+import UsersSpecialList from "../../containers/UsersSpecialList/index";
+import CurrentObjectItem from "../../containers/CurrentObjectItem/index";
 
-import './styles.css';
+import "./styles.css";
 
 // redux:
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-// import { createObject } from '../../../actions/index';
-
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { updateUser } from "../../../actions/index";
 
 class CurrentObjectTab extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            showObjectInfo: false,
-            showUserInfo: false,
-            chosenUser: {}
-        }
-    }
- 
-  
-  // select object:
-  selectObject = (id) => {
-    this.setState({
-        showObjectInfo: !this.state.showObjectInfo  
-    });
+    this.state = {
+      showObjectInfo: false,
+      showUserInfo: false
+    };
   }
+
+  checkUserAssignedToTable(userId) {
+    const { objects } = this.props;
+    for (let obj of objects) {
+      if (obj.userId === userId) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // select object:
+  selectObject = id => {
+    this.setState({
+      showObjectInfo: !this.state.showObjectInfo
+    });
+  };
+
+  // open changing user panel:
+  openChangeUserPanel = (id) => {
+    const { selectedObjectId } = this.props;
+    if ( selectedObjectId === '' ) {
+        alert("ОШИБКА: ОБЪЕКТ НЕ ВЫБРАН! Щелкните на одном из объектов!");
+    } else {
+        this.setState({
+            showUserInfo: !this.state.showUserInfo
+        });
+    }
+    
+  };
 
   // select user:
-  selectUser = (id) => {
-    const newUser = this.props.users.find((user) => (user.id === id));
+  selectUser = newUserId => {
+    const { actions, selectedObjectId } = this.props;
 
-    this.setState({
-        showUserInfo: !this.state.showUserInfo,
-        chosenUser: newUser
-    });
-  }
-
-  
-  render() {
-
-    const { selectedObjectId, objects, users } = this.props;
-
-    const requiredObject = objects.find( (val) => (val.id === selectedObjectId) );
-    var requiredUser;
-    if ( requiredObject !== undefined ) {
-        requiredUser = users.find( (val) => (val.id === requiredObject.userId) );
-    
+    if ( this.checkUserAssignedToTable(newUserId) ) {
+      alert("ОШИБКА: ПОЛЬЗОВАТЕЛЬ УЖЕ ПРИВЯЗАН К СТОЛУ! Выберите другого пользователя!");
+    } else {
+      const newObjData = {
+        id: selectedObjectId,
+        userId: newUserId
+      };
+      actions.updateUser(newObjData);
     }
 
+    this.setState({
+        showUserInfo: !this.state.showUserInfo
+    });
+  };
+
+  render() {
+    const { selectedObjectId, objects, users } = this.props;
+
+    const requiredObject = objects.find(val => val.id === selectedObjectId);
+    var requiredUser = {
+      title: "Not assigned",
+      capability: ""
+    };
+    if (requiredObject !== undefined) {
+      // объект определен
+
+      if (requiredObject.userId !== undefined && requiredObject.userId !== "") {
+        // к объекту привязан пользователь
+        requiredUser = users.find(val => val.id === requiredObject.userId);
+      }
+    } // иначе - либо объект не определен, либо к нему не привязан пользователь
 
     return (
-      <div style={{
-          width: '100%', 
-          display: 'flex', 
-          flexDirection: 'column', 
-          justifyContent: 'center',
-          alignItems: 'center'
-
-      }}>
-        <label className="labelCurrObj">Выбранный объект #ID: {selectedObjectId}</label>
-        <CurrentObjectItem  
-            object={requiredObject}
-            isSelected={false}
-            onClick={this.selectObject} 
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <label className="labelCurrObj">
+          Выбранный объект #ID: {selectedObjectId}
+        </label>
+        <CurrentObjectItem
+          object={requiredObject}
+          isSelected={false}
+          onClick={this.selectObject}
         />
         <label className="labelCurrObj">Информация о пользователе:</label>
-        {
-            requiredObject !== undefined && requiredUser !== undefined &&
-            <UserSpecialItem  
-                user={requiredUser}
-                isSelected={false}
-                onClick={this.selectUser} 
-            />
-        }
-        { 
-            this.state.showUserInfo &&
-            <UsersSpecialList
-                onUserClick={this.selectUser}
-            />
-        }
-        
+        <UserSpecialItem
+            user={requiredUser}
+            isSelected={false}
+            onClick={this.openChangeUserPanel}
+        />
+        {this.state.showUserInfo && (
+          <UsersSpecialList onUserClick={this.selectUser} />
+        )}
       </div>
     );
   }
 }
 
 //for redux:
-const mapStateToProps = (state) => ({
-    objects: state.objects,
-    users: state.users
+const mapStateToProps = state => ({
+  objects: state.objects,
+  users: state.users
 });
-  
-const mapDispatchToProps = (dispatch) => ({
-    actions: bindActionCreators({  }, dispatch)
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators({ updateUser }, dispatch)
 });
-  
-  
+
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(CurrentObjectTab);

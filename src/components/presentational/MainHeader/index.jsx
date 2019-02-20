d7f031f69ca64e9aa38ecca5c644a3d4a7cf9b46
TimmyGOGO <temich941@mail.ru>
2019-02-20 16:01:22 +0300

    Script in package.json for build + editing dependencies

--- src/components/presentational/MainHeader/index.jsx
+++ src/components/presentational/MainHeader/index.jsx
@@ -1,27 +1,25 @@
-import * as React from 'react';
+import React from 'react';
 import './cssans.min.css';
 
-export default class MainHeader extends React.Component {
-  render() {
-    return (
-      <div className="cssans cssans--center">
-        <div className="cssans__accessible">CSSans Pro</div>
+const MainHeader = () => (
+  <div className="cssans cssans--center">
+    <div className="cssans__accessible">CSSans Pro</div>
 
-        <div className="cssans__word">
-          <b className="cssans:o" />
-          <b className="cssans:f" />
-          <b className="cssans:f" />
-          <b className="cssans:i" />
-          <b className="cssans:c" />
-          <b className="cssans:e" />
-        </div>
+    <div className="cssans__word">
+      <b className="cssans:o" />
+      <b className="cssans:f" />
+      <b className="cssans:f" />
+      <b className="cssans:i" />
+      <b className="cssans:c" />
+      <b className="cssans:e" />
+    </div>
 
-        <div className="cssans__word">
-          <b className="cssans:m" />
-          <b className="cssans:a" />
-          <b className="cssans:p" />
-        </div>
-      </div>
-    );
-  }
-}
+    <div className="cssans__word">
+      <b className="cssans:m" />
+      <b className="cssans:a" />
+      <b className="cssans:p" />
+    </div>
+  </div>
+);
+
+export default MainHeader;
